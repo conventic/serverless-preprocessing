3bb3bfffd9df0c81659eeb45d83b73ad100516e1
Clemens Peters <13015002+clemenspeters@users.noreply.github.com>
2023-06-08 13:17:44 +0200

    ✨ Add lambda publisher python code.

---
+++ lambda/publishLinkToSNS/lambda_function.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+import json
+import urllib
+import boto3
+
+print('Loading message function...')
+topc_arn = 'arn:aws:sns:eu-central-1:710731193510:preprocessor'
+sns = boto3.client('sns')
+
+base_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/'
+years = range(2009, 2023)
+months = range(1,13)
+is_dry_run = False
+
+
+def publish(subject, msg):
+    sns.publish(
+        TopicArn=topc_arn,
+        Subject=subject,
+        Message=msg
+    )
+
+def iterate_files():
+    for year in years:
+        for month in months:
+            if year == 2023 and month > 2: # 2023-02 is the latest dataset.
+                return
+            formatted_month = "{:02d}".format(month) # Add leading zeroes
+            dataset_name = "yellow_tripdata_{}-{}".format(year, formatted_month)
+            filename = "{}.parquet".format(dataset_name)
+            url = "{}{}".format(base_url, filename)
+            if not is_dry_run:
+                publish(filename, url)
+            print('Sent: ', filename, url, topc_arn)
+
+def lambda_handler(event, context):
+    iterate_files()
+    if is_dry_run:
+        print ('Dry run complete. No messages published.')
+        return
+    print ('Sent all messages for yellow taxi to SNS.')
