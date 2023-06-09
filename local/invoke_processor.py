843cf615dfae8a8e2a1124f6bd72c5cbcd3bdc9f
Clemens Peters <13015002+clemenspeters@users.noreply.github.com>
2023-06-09 12:54:50 +0200

    ✨ Add working local invoker.

--- local/invoke_processor.py
+++ local/invoke_processor.py
@@ -2,12 +2,29 @@ from __future__ import print_function
 
 import json
 import urllib
+import datetime
+from lambda_functions.preprocessor.lambda_function import handler
 
 print('Processing local files...')
 
 base_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/'
 years = range(2009, 2023)
 months = range(1,13)
+
+
+def invoke_processor(file_name):
+    # Define the payload for the Lambda function
+    payload = {
+        "Records": [
+            {
+                "Sns": {
+                    "Message": file_name
+                }
+            }
+        ]
+    }
+    handler(payload, {})
+    print("Invoked preprocess function for file_name: ", file_name)
 
 
 def iterate_files():
@@ -17,9 +34,11 @@ def iterate_files():
                 return
             formatted_month = "{:02d}".format(month) # Add leading zeroes
             dataset_name = "yellow_tripdata_{}-{}".format(year, formatted_month)
-            filename = "{}.parquet".format(dataset_name)
-            url = "{}{}".format(base_url, filename)
-            print('Process: ', filename, url)
-            # TODO: invoke actual processor with filename
+            file_name = "{}.parquet".format(dataset_name)
+            url = "{}{}".format(base_url, file_name)
+            print('Process: ', file_name, url)
+            invoke_processor(file_name)
 
+print("Start time: ", datetime.datetime.now())
 iterate_files()
+print("End time: ", datetime.datetime.now())
